12f109a3938915255b4ebe5a39c1dcef59f36de0
yoshi-miyazaki <69875815+yoshi-miyazaki@users.noreply.github.com>
2021-07-02 11:31:57 -0400

    Add files via upload

--- dust_diffuse.py
+++ dust_diffuse.py
@@ -4,6 +4,67 @@ import sys
 def nds(nu, dist, sigma, i):
     return nu[i]*dist[i]*sigma[i]
 
+
+def calc_gas_evol(sigma, sigma_d, nu, vn, dist, dt):
+    n  = len(sigma)
+    Ag = np.empty(n)
+    Bg = np.empty(n)
+    Cg = np.empty(n)
+
+    div = 10
+    dt1 = dt/div
+    dr  = dist[1] - dist[0]
+
+    for j in range(div):
+        # A(i,i)S(i,j+1) = S(i,j)     ... Ad(i)
+        for i in range(n):
+            if (i<(n-1)):
+                Ag[i] = (-dt1/(dr*dr) * (0.5*nds(nu,dist,sigma,i-1) + nds(nu,dist,sigma,i) +0.5*nds(nu,dist,sigma,i+1))/sigma[i]) / dist[i]
+            else:
+                Ag[i] = (-dt1/(dr*dr) * (0.5*nds(nu,dist,sigma,i-1) + nds(nu,dist,sigma,i))/sigma[i]) / dist[i]
+
+            # add advection term, considering upwind scheme
+            if (vn[i]<0):
+                Ag[i] += vn[i]*dt1/dr
+                
+            if (i<(n-1)):
+                if (vn[i+1] > 0):
+                    Ag[i] -= vn[i+1]*dt1/dr
+
+        # A(i+1,i)S(i+1,j+1) = S(i,j) ... Bd(i)
+        for i in range(n-1):
+            Bd[i] =  (dt1/(dr*dr) * 0.5*(nds(nu,dist,sigma,i) + nds(nu,dist,sigma,i+1))/sigma[i+1]) / dist[i]
+        
+            if (vn[i+1] < 0):
+                Bg[i] -= (dist[i+1]/dist[i]) * vn[i+1]*dt1/dr
+        Bg[-1] = 0.0
+            
+        # A(i-1,i)S(i-1,j+1) = S(i,j) ... Cd(i)
+        for i in range(1,n):
+            Cg[i] =  dt1/(dr*dr) * 0.5*(nds(nu,dist,sigma,i-1) + nds(nu,dist,sigma,i))/ (sigma[i-1]*dist[i])
+
+            if (vn[i]>0):
+                Cg[i] += (dist[i-1]/dist[i]) * vn[i]*dt1/dr
+
+        #print("B", sigma_d[-5:])
+        #print("b", sigma[-5:])
+        #print("Az", Ad[-5:])
+        #print("Bz", Bd[-5:])
+        #print("Cz", Cd[-5:])
+        sigma_g = solve_Crank_Nicolson(Ad, Bd, Cd, sigma_g)
+        # repeat for div times
+        #print("A", sigma_d[-5:])
+        #print("a", sigma[-5:])
+        #print(nu[-5:])
+        #print()
+
+        if (np.isnan(sigma_d[-1])):
+            sys.exit()
+        
+    # return
+    return sigma_g
+
+    
 def calc_dust_evol(sigma, sigma_d, nu, vn, dist, dt):
     n  = len(sigma)
     Ad = np.empty(n)
